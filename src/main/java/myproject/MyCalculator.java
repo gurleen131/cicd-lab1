fe04a8391bd406cf7f3f0f77b3f2d906afdba910
gurleen131 <grlnsethi007@gmail.com>
2023-08-25 09:47:48 +0530

    commited

---
+++ src/main/java/myproject/MyCalculator.java
@@ -0,0 +1,19 @@
+package myproject;
+
+public class MyCalculator {
+	public int sum(int a, int b) 
+	{
+		return(a+b);
+	}
+
+	public int diff(int a, int b)
+	{
+			return(a-b);
+	}	
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
