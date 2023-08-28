037dc966724a96fd6a91df6fbc96901380522476
gurleen131 <grlnsethi007@gmail.com>
2023-08-28 12:56:00 +0530

    new commit

--- src/main/java/myproject/MyCalculator.java
+++ src/main/java/myproject/MyCalculator.java
@@ -12,6 +12,12 @@ public class MyCalculator {
 	}	
 
 	public static void main(String[] args) {
+		MyCalculator cal = new MyCalculator();
+		System.out.println("sum is : "+ cal.sum(20, 10));
+		System.out.println("diff is : "+ cal.diff(20, 10));
+		
+
+				
 		
 		
 	}
